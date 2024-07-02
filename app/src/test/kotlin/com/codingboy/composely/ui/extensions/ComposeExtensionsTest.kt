d755ee42de629cee59a21d8c492ef7baceaee602
jeongmin <jjungmin@gmail.com>
2024-07-02 14:39:07 +0900

    [#4] Modifier extension function 'applyIf' and corresponding unit tests

---
+++ app/src/test/kotlin/com/codingboy/composely/ui/extensions/ComposeExtensionsTest.kt
@@ -0,0 +1,29 @@
+package com.codingboy.composely.ui.extensions
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.junit.Assert
+import org.junit.Test
+
+class ComposeExtensionsTest {
+
+    @Test
+    fun applyIfAppliesModifierWhenConditionIsTrue() {
+        val originalModifier = Modifier
+        val newModifier = Modifier.padding(10.dp)
+
+        val result = originalModifier.applyIf(true) { newModifier }
+        Assert.assertNotEquals(result, originalModifier)
+    }
+
+    @Test
+    fun applyIfDoesNotApplyModifierWhenConditionIsFalse() {
+        val originalModifier = Modifier
+        val newModifier = Modifier.padding(10.dp)
+
+        val result = originalModifier.applyIf(false) { newModifier }
+
+        Assert.assertEquals(originalModifier, result)
+    }
+}
